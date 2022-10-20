c751e90c6936779a5eb88da26d9e3535eb6a991d
newkayak12 <newkayak12@gmail.com>
2022-10-20 22:48:21 +0900

    javaInAction

--- src/Chapter_09_리팩토링_테스팅_디버깅/Section1.java
+++ src/Chapter_09_리팩토링_테스팅_디버깅/Section1.java
@@ -1,14 +1,18 @@
 package Chapter_09_리팩토링_테스팅_디버깅;
 
+import org.junit.jupiter.api.Test;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class Section1 {
@@ -341,6 +345,231 @@ public class Section1 {
      *
      *
      *              > 9.2.4 의무 체인
+     *
+     *  작업 처리 객체의 체이닝을 만들 때 의무 체인 패턴을 사용한다. 한 객체가 어떤 작업을 처리한 다음에 다른 객체로 결과를 전달하고, 다른 객체도 해야할 작업을
+     *  다 처리한 다음 다른 객체로 전달하는 식이다. 일반적으로 다음으로 처리할 객체 정보를 유지하는 필드를 포함하는 작업 처리 추상 클래스로 의무 체인 패턴을
+     *  구성한다. 작업 처리 객체가 자신의 작업을 끝냈으면 다음 작업 처리 객체로 결과를 전달한다.
+     */
+    public abstract class ProcessingObject<T>{
+        protected ProcessingObject<T> successor;
+        public void setSuccessor(ProcessingObject<T> successor){
+            this.successor = successor;
+        }
+        public T handle(T input){
+            T r = handleWork(input);
+            if(successor != null){
+                return successor.handle(r);
+            }
+            return r;
+        }
+        abstract protected T handleWork(T input);
+    }
+    /**
+     *   잘 생각해보면 템플릿 메소드 디자인 패턴이 사용되었음을 알 수 있다. handle 메소드는 일부 작업을 어떻게 처리해야 할지 전체적으로 기술한다.
+     *   Processing Object 클래스를 상속받아 handleWork 메소드를 구현해야 다영한 종류의 작업 처리 객체를 만들 수 있다.
+     *
+     *   이 패턴을 어떻게 상요할지 예시를 보자.
+     */
+    public class HeaderTextProcessing extends ProcessingObject<String>{
+        @Override
+        protected String handleWork(String text) {
+            return "From Raul, Mario and Alan: "+ text;
+        }
+    }
+    public class SpellCheckerProcessing extends ProcessingObject<String>{
+        @Override
+        protected String handleWork(String text) {
+            return text.replaceAll("labda", "lambda");
+        }
+    }
+    /**
+     * 두 작업 처리 객체를 연결해서 작업 체인을 만들 수 있다.
+     */
+    ProcessingObject<String> p1 = new HeaderTextProcessing();
+    ProcessingObject<String> p2 = new SpellCheckerProcessing();
+    {
+        p1.setSuccessor(p2);
+        String result = p1.handle("Aren 't labdas really sexy?1");
+        System.out.println(result);
+    }
+    /**
+     *          > 람다 표현식 사용
+     *  작업 처리 객체를 Function<String,String>, 더 정확히 표현하자면 UnaryOperator<String> 형식의 인스턴스로 표현할 수 있다. andThen
+     *  메소드로 이들 함수를 조합해서 체인을 만들 수 있다.
+     *
+     *  unary > 단항
+     */
+    UnaryOperator<String> headerProcessing = (String text) -> "From Raul, Mario and Alan : " + text;
+    UnaryOperator<String> spellCheckerProcessing = (String text) -> text.replace("labda", "lambda");
+    Function<String, String> pipeline = headerProcessing.andThen(spellCheckerProcessing);
+    String result = pipeline.apply("Aren't labdas really sexy?!!");
+    /**
+     *
+     *          > 9.2.5 팩토리
+     *  인스턴스화 로직을 클라이언트에 노출하지 않고 객체를 만들 때 팩토리 디자인 패턴을 사용한다. 예를 들어 우리가 은행에서 일하고 있다고 하면 대출, 채권,
+     *  주식 등 다양한 상품을 만들어야 한다고 가정하자. 그럼 상품을 만들어주는 것을 담당하는 Factory 클래스가 필요하다.
+     */
+    public interface Product{
+        public void earnMoney();
+    }
+    public class Loan implements Product{
+        @Override
+        public void earnMoney() {
+
+        }
+    }
+    public class Stock implements Product{
+
+        @Override
+        public void earnMoney() {
+
+        }
+    }
+    public class Bond implements Product{
+
+        @Override
+        public void earnMoney() {
+
+        }
+    }
+    public class ProductFactory {
+        public /*static*/ Product createProduct(String name){
+            switch (name){
+                case "loan" :return new Loan();
+                case "stock" :return new Stock();
+                case "bond" :return new Bond();
+                default: throw new RuntimeException("No such Product "+name);
+            }
+        }
+
+    }
+    /**
+     * 여기서 Loan, Stock, Bond 모두 Product의 서브 형식이다. createProduct 메소드는 생산된 상품을 설정하는 로직을 포함할 수 있다.
+     * 이는 부가적인 기능일 뿐 위 코드의 진짜 장점은 생성자와 설정을 외부로 노출하지 않음으로써 클라이언트가 단순하게 상품을 생산할 수 있다는 것이다.
+     */
+    Product p = new ProductFactory().createProduct("loan");
+    /**
+     *      > 람다 표현식
+     *  생성자도 메소드 참조처럼 접근할 수 있다. 아래는 Loan을 생성하는 코드이다.
+     */
+    Supplier<Product> loanSupplier = Loan::new;
+    Product loan = loanSupplier.get();
+    /**
+     * 상품명을 생성자로 연결하는 Map을 만들어서 코드를 재구현할 수도 있다.
+     */
+   Map<String, Supplier<Product>> productList = Map.of("loan", Loan::new, "stock", Stock::new, "bond", Bond::new);
+    /**
+     * 이제 Map을 이용해서 팩토리 디자인 패턴에서 했던 것처럼 다양한 상품을 인스턴스화 할 수 있다.
+     */
+    public Product createProductLambda( String name ){
+        Supplier<Product> p = productList.get(name);
+        if( p != null ) return p.get();
+        throw new IllegalArgumentException("No such product " + name);
+    }
+    /**
+     * 팩토리 패턴이 수행하던 작업이 자바 8의 새로운 기능으로 깔끔하게 정리했다. 하지만 팩토리 메소드 createProduct가 상품 생성자로 여러 인수를 전달하는 상황
+     * 에서는 이 기법을 적용하기가 어렵다. 단순한 Supplier 함수형 인터페이스로는 이 문제를 해결할 수 없다.
+     *
+     * 예를 들어 세 인수 (Integer 둘, 문자열 하나)를 받는 상품의 생성자가 있다고 가정하자. 세 인수를 지원하려면 TriFunction이라는 특별한 함수형 인터페이스를
+     * 만들어야 한다. 결국 다음 Map의 시그니쳐가 복잡해진다.
+     */
+    public interface TriFunction<T,U,V,R>{
+        R apply(T t, U u, V v);
+    }
+    Map<String, TriFunction<Integer, Integer, String, Product>> map = new HashMap<>();
+    /**
+     *  지금까지 람다 표현식으로 코드 리팩토링하는 방법을 살펴봤다. 이번에는 새로 구현한 코드가 올바른지 어떻게 검증할 수 있는지 알라보자.
+     *
+     *
+     *              > 9.3 람다 테스팅
+     *  이제 람다 표현식을 실무에 적용해서 멋지고 간단한 코드를 구현할 수 있다. 하지만 개발자의 최종 업무 목표는 제대로 작동하는 코드를 구현하는 것이지
+     *  깔끔한 코드를 구현하는 것이 아니다.
+     *
+     *  일반적으로 좋은 소프트웨어 공학자라면 프로그램이 의도대로 동작하는지 확인할 수 있는 단위 테스팅 을 진행한다. 우리는 소스 코드가 일부가 예산된 결과를
+     *  도출할 것이라 단언하는 테스트 케이스를 구현한다.
+     */
+    public class Point{
+        private final int x;
+        private final int y;
+        private Point (int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+        public int getX(){return x;}
+        public int getY(){return y;}
+        public Point moveRightBy(int x){
+            return new Point(this.x + x, this.y);
+        }
+    }
+    @Test
+    public void testMoveRightBy() throws Exception{
+        Point p1 = new Point(5,5);
+        Point p2 = p1.moveRightBy(10);
+        assertEquals(15, p2.getX());
+        assertEquals(5,p2.getY());
+    }
+    /**
+     *          > 9.3.1 보이는 람다 표현식의 동작 테스팅
+     *   moveRightBy는 public이므로 위 코드는 문제 없이 작동한다. 하지만 람다는 익명함수 이므로 테스트 코드 이름을 호출할 수 없다.
+     *   따라서 필요하면 람다를 필드에 저장해서 재사용할 수 있으며, 람다의 로직을 테스트할 수 있다. 메소드를 호출하는 것처럼 람다를 사용할 수 있다.
+     *   예를 들어 Point 클래스에 compareByXAndThenY라는 정적 필드를 추가했다고 가정하자. (compareByXAndThenY를 사용하면 메소드 참조로 생성한
+     *   Comparator에 접근할 수 있다.)
+     *
+     *
+     *      public class Point {
+     *          public final static Comparator<Point> compareByXAndThenY = comparing(Poin::getX).thenComparing(Point::getY)
+     *      }
+     *
+     *   람다 표현식은 함수형 인터페이스의 인스턴스를 생성한다는 사실을 기억하자 따라서 생성된 인스턴스의 동작으로 람다 표현식을 테스트할 수 있다.
+     *   다음은 Comparator 객체 compareByXAndThenY에 다양한 인수로 compare 메소드를 호출하면서 예산대로 동작하는지 테스트하는 코드이다.
+     *
+     *     @Test
+     *     public void testComparingTwoPoints() throws Exception{
+     *         Point p1 = new Point(10, 15);
+     *         Point p2 = new Point(10,20);
+     *         int result = Point.compareByXAndThenY.compare(p1,p2)
+     *         assertTrue(result < 0);
+     *     }
+     *
+     *
+     *              > 9.3.2 람다를 사용하는 메소드 동작에 집중하자
+     *   람다의 목표는 정해진 동작을 다른 메소드에서 사용할 수 있도록 하나의 조각으로 캡슐화하는 것이다. 그러려면 세부 구현을 하는 람다 표현식을 공개하지
+     *   않아야 한다. 람다 표현식을 사용하는 메소드의 동작을 테스트함으로써 람다를 공개하지 않으면서 람다 표현식을 검증할 수 있다.
+     *
+     *      public static List<Point> moveAllPointsRighBy(List<Point> points, int x_ {
+     *          return point.stream().map(p->new Point(p.getX() + x, p.getY())).collect(toList());
+     *      }
+     *
+     *   위 코드에서 p->new Point(p.getX() + x, p.getY())를 테스트 하는 부분은 없다. 그러나 메소드로서 동작을 확인할 수 있다.
+     *
+     *
+     *              > 9.3.3 복잡한 람다를 개별 메소드로 분할하기
+     *    테스트 코드에서 람다 표현식을 참조할 수 없다는 문제점에 직면하게 될 것이다. 그럼 복잡한 람다 표현식을 어떻게 테스트할 것인가? 해답은
+     *    람다 표현식을 메소드 참조로 바꾸는 것이다. 그러면 일반 메소드를 테스트하듯이 람다 표현식을 테스트할 수 있다.
+     *
+     *
+     *              > 9.3.4 고차원 함수 테스팅
+     *     함수를 인수로 받거나 다른 함수를 반환하는 메소드(이를 고차원 함수라고 한다.)는 조금 더 사용하기 까다롭다. 메소드가 람다를 인수로 받는다면 다른
+     *     람다로 메소드의 동작을 테스트할 수 있다.
+     *
+     *
+     *              > 9.4 디버깅
+     *     문제가 발생한 코드를 디버깅할 때 두 가지를 먼저 확인해야 한다.
+     *      1. 스택 트레이스 : 어디서 멈췄고 왜 멈췄는지를 확인해야한다. 그러나 람다 표현식은 이름이 없기 때문에 복잡한 스택트레이스가 생성된다.
+     */
+    {
+        List<Point> points = Arrays.asList(new Point(12, 2), null);
+        points.stream().map(p->p.getX()).forEach(System.out::println);
+    }
+    /**
+     *
+     *      stackTrace를 살펴보면
+     *              at Debugging.lambda$main$0(Debugging java:6) at Debugging$$Lambda$5/284720968.apply(Unknown.Source)
+     *       라고 되어 있는데, (Debugging java:6)의 왼쪽 $0는 무슨 뜻일까? 메소드 참조를 사용하는 클래스와 같은 곳에 선언되어 있는 메소드를 참조
+     *       할 때는 메소드 참조 이름이 스택트레이스에 나타난다. 이러한 이유로 스택트레이스으로는 한계가 있다.
+     *
+     *
+     *      2. 로깅 : stream 등에서 peek 등으로 로깅을 하는 것으로 확인할 수 있다.
      */
 
 
